7724c5da3bcab4cc2d30b856b16d7e880a3a570b
Alexander Bispo <alexbispo173@gmail.com>
2019-10-25 01:34:51 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -5,16 +5,18 @@ attr_accessor :name, :years
 
 def initialize(name,years)
   @name = name
-  @years = years
-  @@all << self
-end
-
-def self.all
-  @@all
-end
-
-def new_meal(customer,total,tip)
-    Meal.new(self,customer,total,tip)
+    @years = years
+   @@all << self
   end
 
+  def self.all
+    @@all
+  end
+  
+  def new_meal(customer,total,tip)
+      Meal.new(self,customer,total,tip)
+    end
+  def meals
+    Meal.all.select{|m| m.customer == self}
+  end
 end
